35c0d408c4dc01810f1a30df3f595aee80c1767d
SandulRusara <sandulrusara8@gmail.com>
2024-10-11 19:41:51 +0530

    item entity add

--- src/main/java/lk/ijse/springassignment/entity/impl/itemEntity.java
+++ src/main/java/lk/ijse/springassignment/entity/impl/itemEntity.java
@@ -1,4 +1,22 @@
 package lk.ijse.springassignment.entity.impl;
 
-public class itemEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.springassignment.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "item")
+public class itemEntity implements SuperEntity {
+    @Id
+    private String itemCode;
+    private String itemName;
+    private int qtyOnHand;
+    private double unitPrice;
 }
