ab1e5d95ceb13f8a8fe5727a782881e639d6c77e
SandulRusara <sandulrusara8@gmail.com>
2024-10-11 23:03:15 +0530

    item dao update

--- src/main/java/lk/ijse/springassignment/service/impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/springassignment/service/impl/ItemServiceImpl.java
@@ -4,7 +4,10 @@ import jakarta.transaction.Transactional;
 import lk.ijse.springassignment.dao.ItemDao;
 import lk.ijse.springassignment.dto.ItemStatus;
 import lk.ijse.springassignment.dto.impl.ItemDTO;
+import lk.ijse.springassignment.entity.impl.ItemEntity;
+import lk.ijse.springassignment.exception.DataPersistException;
 import lk.ijse.springassignment.service.ItemService;
+import lk.ijse.springassignment.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +17,14 @@ import java.util.List;
 public class ItemServiceImpl implements ItemService {
     @Autowired
     private ItemDao itemDao;
-
+    @Autowired
+    private Mapping mapping;
     @Override
     public void saveItem(ItemDTO itemDTO) {
-
+        ItemEntity item=itemDao.save(mapping.toItemEntity(itemDTO));
+        if (item==null){
+            throw new DataPersistException("Item not saved");
+        }
     }
 
     @Override
