1132dac80c53ea682840ef5b1c30eded3f8c5dde
SandulRusara <sandulrusara8@gmail.com>
2024-10-11 19:47:05 +0530

    item dto add

---
+++ src/main/java/lk/ijse/springassignment/dto/ItemStatus.java
@@ -0,0 +1,6 @@
+package lk.ijse.springassignment.dto;
+
+import java.io.Serializable;
+
+public interface ItemStatus extends Serializable,SuperDTO {
+}
---
+++ src/main/java/lk/ijse/springassignment/dto/impl/itemDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.springassignment.dto.impl;
+
+import lk.ijse.springassignment.dto.ItemStatus;
+import lk.ijse.springassignment.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class itemDTO  implements SuperDTO, ItemStatus {
+    private String itemCode;
+    private String itemName;
+    private int qtyOnHand;
+    private double unitPrice;
+}
