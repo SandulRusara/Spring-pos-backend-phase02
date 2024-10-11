025183899e155c8b851549f351a3a4fb090bdeb6
SandulRusara <sandulrusara8@gmail.com>
2024-10-11 22:58:55 +0530

    item controller update

--- src/main/java/lk/ijse/springassignment/controller/itemController.java
+++ src/main/java/lk/ijse/springassignment/controller/itemController.java
@@ -1,14 +1,33 @@
 package lk.ijse.springassignment.controller;
 
+import lk.ijse.springassignment.dto.impl.ItemDTO;
+import lk.ijse.springassignment.exception.DataPersistException;
+import lk.ijse.springassignment.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/item")
 @CrossOrigin
 public class itemController {
+    @Autowired
+    private ItemService itemService;
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void>saveItem(@RequestBody ItemDTO itemDTO){
+        itemDTO.setItemCode(itemDTO.getItemCode());
+        try {
+            itemService.saveItem(itemDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
