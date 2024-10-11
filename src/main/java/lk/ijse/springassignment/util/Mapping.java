aa5d4a17dc75703c7395b51415e8fd058d96856f
SandulRusara <sandulrusara8@gmail.com>
2024-10-11 22:50:34 +0530

    moddle mapper item update

--- src/main/java/lk/ijse/springassignment/util/Mapping.java
+++ src/main/java/lk/ijse/springassignment/util/Mapping.java
@@ -1,6 +1,8 @@
 package lk.ijse.springassignment.util;
 import lk.ijse.springassignment.dto.impl.CustomerDTO;
+import lk.ijse.springassignment.dto.impl.ItemDTO;
 import lk.ijse.springassignment.entity.impl.CustomerEntity;
+import lk.ijse.springassignment.entity.impl.ItemEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,18 @@ public class Mapping {
     }
     public List<CustomerDTO> asCustomerDTOList(List<CustomerEntity> customerEntities) {
         return modelMapper.map(customerEntities, new TypeToken<List<CustomerDTO>>() {}.getType());
+    }
+
+
+
+    public ItemEntity toItemEntity(ItemDTO itemDTO){
+        return modelMapper.map(itemDTO, ItemEntity.class);
+    }
+    public ItemDTO toItemDTO(ItemEntity itemEntity){
+        return modelMapper.map(itemEntity, ItemDTO.class);
+    }
+    public List<ItemDTO>toItemList(List<ItemEntity>itemList){
+        return modelMapper.map(itemList,new TypeToken<List<ItemDTO>>(){}.getType());
     }
 
 }
