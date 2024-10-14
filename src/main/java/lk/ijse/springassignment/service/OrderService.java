88df6c19cbc3c32f445cf0d1a890cc67d0d76b06
SandulRusara <sandulrusara8@gmail.com>
2024-10-14 10:37:25 +0530

    order service add

--- src/main/java/lk/ijse/springassignment/service/OrderService.java
+++ src/main/java/lk/ijse/springassignment/service/OrderService.java
@@ -1,5 +1,14 @@
 package lk.ijse.springassignment.service;
 
+import lk.ijse.springassignment.dto.OrderStatus;
+import lk.ijse.springassignment.dto.impl.OrderDTO;
+
+import java.util.List;
+
 public interface OrderService {
-    void saveOrder()
+    void saveOrder(OrderDTO orderDTO);
+    void updateOrder(String orderId,OrderDTO orderDTO);
+    void deleteOrder(String orderId);
+    OrderStatus getOrder(String orderId);
+    List<OrderDTO>getAllOrder();
 }
