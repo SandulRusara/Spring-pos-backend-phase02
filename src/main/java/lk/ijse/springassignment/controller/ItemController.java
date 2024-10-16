58b01e69d3b9b7c6a52e47e4e9e835b63616f057
SandulRusara <sandulrusara8@gmail.com>
2024-10-16 12:52:31 +0530

    logger save info add

--- src/main/java/lk/ijse/springassignment/controller/ItemController.java
+++ src/main/java/lk/ijse/springassignment/controller/ItemController.java
@@ -6,29 +6,32 @@ import lk.ijse.springassignment.dto.impl.ItemDTO;
 import lk.ijse.springassignment.exception.DataPersistException;
 import lk.ijse.springassignment.service.ItemService;
 import lk.ijse.springassignment.util.RegexProcess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpClient;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/item")
 @CrossOrigin
-public class itemController {
+public class ItemController {
     @Autowired
     private ItemService itemService;
-
+    static Logger logger = LoggerFactory.getLogger(ItemController.class);
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void>saveItem(@RequestBody ItemDTO itemDTO){
         itemDTO.setItemCode(itemDTO.getItemCode());
         try {
             itemService.saveItem(itemDTO);
+            logger.info("Item saved successfully with item code : ",itemDTO.getItemCode());
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
+
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
             e.printStackTrace();
