d93bcece0b659f2c7cd32bbc44d2a41917b10dac
SandulRusara <sandulrusara8@gmail.com>
2024-10-16 16:30:14 +0530

    logger add orderdetails ilmp

--- src/main/java/lk/ijse/springassignment/service/impl/OrderDetailsServiceImpl.java
+++ src/main/java/lk/ijse/springassignment/service/impl/OrderDetailsServiceImpl.java
@@ -7,6 +7,8 @@ import lk.ijse.springassignment.entity.impl.OrderDetailsEntity;
 import lk.ijse.springassignment.exception.DataPersistException;
 import lk.ijse.springassignment.service.OrderDetailsService;
 import lk.ijse.springassignment.util.Mapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +19,16 @@ public class OrderDetailsServiceImpl implements OrderDetailsService {
     private OrderDetailsDao orderDetailsDao;
     @Autowired
     private Mapping mapping;
+    Logger logger= LoggerFactory.getLogger(OrderDetailsServiceImpl.class);
     @Override
     public void saveOrderDetails(OrderDetailsDTO orderDetailsDTO) {
+        logger.info("Attempting to save orderdetails with Id ",orderDetailsDTO.getId());
         OrderDetailsEntity save = orderDetailsDao.save(mapping.toOrderDetailsEntity(orderDetailsDTO));
         if (save==null){
+            logger.error("Order Details not saved with Id ",orderDetailsDTO.getId());
             throw new DataPersistException("order details not saved!!!!!!!");
+        }else {
+            logger.info("Orderdetails saved Successfully with Id "+orderDetailsDTO.getId());
         }
     }
 }
