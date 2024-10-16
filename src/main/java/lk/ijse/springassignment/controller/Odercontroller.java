f5efad6cf6b832f20cc0354e58904ba6f5539288
SandulRusara <sandulrusara8@gmail.com>
2024-10-16 15:49:28 +0530

    logger info order controller add

--- src/main/java/lk/ijse/springassignment/controller/Odercontroller.java
+++ src/main/java/lk/ijse/springassignment/controller/Odercontroller.java
@@ -6,6 +6,8 @@ import lk.ijse.springassignment.dto.impl.OrderDTO;
 import lk.ijse.springassignment.exception.DataPersistException;
 import lk.ijse.springassignment.service.OrderService;
 import lk.ijse.springassignment.util.RegexProcess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,15 +22,19 @@ import java.util.List;
 public class Odercontroller {
     @Autowired
     private OrderService orderService;
+    static Logger logger= LoggerFactory.getLogger(Odercontroller.class);
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveOrder(@RequestBody OrderDTO orderDTO){
         orderDTO.setOrderId(orderDTO.getOrderId());
         try {
             orderService.saveOrder(orderDTO);
+            logger.info("Saved Order !!");
             return new  ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
+            logger.info("Bad Request !!");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
+            logger.info("Internal Server Erro");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
