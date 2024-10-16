9a31c4f6e66464cab96887ad334ce36b605f7a48
SandulRusara <sandulrusara8@gmail.com>
2024-10-16 08:48:09 +0530

    orderDetail dto add

---
+++ src/main/java/lk/ijse/springassignment/dto/impl/OrderDetailsDTO.java
@@ -0,0 +1,25 @@
+package lk.ijse.springassignment.dto.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderDetailsDTO {
+    private String id;
+    private LocalDate date;
+    private String customerName;
+    private String customerCity;
+    private String customerTel;
+    private String itemName;
+    private int orderQty;
+    private double unitPrice;
+    private ItemDTO item;
+    private OrderDTO order;
+
+
+}
